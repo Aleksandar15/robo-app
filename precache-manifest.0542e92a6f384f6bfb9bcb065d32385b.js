d82bb40abc3db6e81ec4f40e7105ef5d98345101
Aleksandar15 <58865226+Aleksandar15@users.noreply.github.com>
2020-09-26 18:35:05 +0200

    Updates

---
+++ precache-manifest.0542e92a6f384f6bfb9bcb065d32385b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c81381f060d3aa68a20973fedfd5373",
+    "url": "/robo-app/index.html"
+  },
+  {
+    "revision": "6684574dffc22b64bed6",
+    "url": "/robo-app/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "76a567152e627a1f7c40",
+    "url": "/robo-app/static/css/main.12e73597.chunk.css"
+  },
+  {
+    "revision": "6684574dffc22b64bed6",
+    "url": "/robo-app/static/js/2.94842557.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/robo-app/static/js/2.94842557.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76a567152e627a1f7c40",
+    "url": "/robo-app/static/js/main.2d80dc58.chunk.js"
+  },
+  {
+    "revision": "acc4aa42bc7fd7b2ec0c",
+    "url": "/robo-app/static/js/runtime-main.abd4af74.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robo-app/static/media/SEGA.12a3db8b.woff"
+  }
+]);
